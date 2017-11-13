0f660e5220edad872b2e6b339530d8824ab966f6
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-13 18:04:46 -0500

    Add models/Page & models/Chapter classes for chapter & page contents.

---
+++ models/Page.php
@@ -0,0 +1,59 @@
+<?php
+
+/* * *********************
+ * 	Project		: ArcGisWebDev
+ * 	Document	: Page.php
+ * 	@author		: Todd Mullen
+ * 	Created		: Nov 13, 2017, 5:35:52 PM
+ * 	Description	:
+ * 		Page
+ * ********************* */
+
+namespace imagi\models;
+
+
+/**
+ * Page
+ */
+class Page
+{
+
+/////Constants/////
+
+	const NAME = "";
+
+/////public vars/////
+
+	public $path = "";
+	public $label = "";
+	public $title = "";
+
+/////private vars/////
+/////public methods/////
+
+
+
+	/**
+	 *
+	 * @return void
+	 * @access public
+	 */
+	public function __construct( $path, $label, $title )
+	{
+		$this->path		= $path;
+		$this->label	= $label;
+		$this->title	= $title;
+	}
+
+	public function getLink(){
+		$title = $this->title ? $this->title : "";
+		$label = "title='" . $this->label ? $this->label : $this->path . "'";
+		return "<a href='$path' $title>$label</a>";
+	}
+
+
+	/////private methods/////
+
+
+}//End Page
+
