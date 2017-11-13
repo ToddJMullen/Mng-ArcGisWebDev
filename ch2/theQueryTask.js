2eb69263662053f3430562eb94c4ba4a62d0b7f9
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-12 19:27:11 -0500

    Add theQueryTask.js, Add change listener to population select & add feature graphics to screen on result.

--- ch2/theQueryTask.js
+++ ch2/theQueryTask.js
@@ -21,19 +21,39 @@ require([
 function setupQueryTaskMap(
 	dom, on, array, Color
 	,Map, Query, QueryTask, SimpleMarkerSymbol
-){console.log("setupQueryTaskMap()");
+){
+	console.log("setupQueryTaskMap().loaded");
+
 	var map = new Map("divMap", {
-		basemap: "streets"
-		,autoResize: true
-		,center: [-118.2095, 34.0866]
-		,zoom: 10
+		basemap		: "streets"
+		,autoResize	: true
+		,center		: [-118.2095, 34.0866]
+		,zoom		: 10
 	})
-	,uri = "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/la_county_labor_centroid/FeatureServer/0"
-	,markerSymbol	= new SimpleMarkerSymbol( SimpleMarkerSymbol.STYLE_SQUARE, 10, null, new Color([50,50,255]) )
+	,selPopulation	= dom.byId("selPopulation")
+	,uri			= "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/la_county_labor_centroid/FeatureServer/0"
+	,markerSymbol	= new SimpleMarkerSymbol( SimpleMarkerSymbol.STYLE_CIRCLE, 20, null, new Color([50,50,255, 0.5]) )
 
 	;
 
-	function onQuerySuccess( featureSet) {
+	on( selPopulation, "change", makeQuery );
+
+
+	function makeQuery(e){
+		var pop = e.target.value;
+		if( pop.length > 0 ){
+			var queryTask	= new QueryTask(uri)
+			,query			= new Query()
+			;
+			query.where = "TOTAL_POP > " + pop
+			query.returnGeometry	= true;
+			queryTask.execute( query ).then( onQuerySuccess, onQueryError );
+		}
+	}//makeQuery
+
+
+
+	function onQuerySuccess( featureSet ) {
 		console.log("onQuerySuccess()", featureSet );
 		map.graphics.clear();
 		//set the marker / symbol for each datum & add it to the map
@@ -47,19 +67,6 @@ function setupQueryTaskMap(
 		console.error("An error has occured: ", err );
 	}
 
-	on( dom.byId("selPopulation"), "change", function makeQuery(e){
-		var pop = e.target.value;
-		if( pop.length > 0 ){
-			var queryTask	= new QueryTask(uri)
-			,query			= new Query()
-			;
-			query.where = "TOTAL_POP > " + pop
-			query.returnGeometry	= true;
-			queryTask.execute( query ).then( onQuerySuccess, onQueryError );
-		}
-	})//on(change)
-
-	console.log("setupQueryTaskMap()");
 }//setupQueryTaskMap()
 
 
