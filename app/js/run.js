a7736e59c8897943cd9fd77ca43c93edb26b51ff
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-16 17:40:29 -0500

    run.js, Add configuration scripts for Dojo loader & custom packages.

--- app/js/run.js
+++ app/js/run.js
@@ -7,7 +7,44 @@
  *      Controls
  ***********************/
 
-/* global  */
+/* global define, require, location */
+/* jshint laxcomma:true */
+(function(){
+	"use strict";
+
+	var pathRegEx	= new RegExp(/\/[^\/]+$/)
+	//define var with adjusted location of app source files
+	,locationPath	= location.pathname.replace( pathRegEx, "" )
+	;
+
+	require({
+		async: true
+		//define name shortcuts for imports(requires)
+		,aliases: [
+			["text", "dojo/text"]
+		]
+		//define/import custom sources
+		,packages: [{
+				name: "controllers"
+				,location: locationPath + "js/controllers"
+			},{
+				name: "services"
+				,location: locationPath + "js/services"
+			},{
+				name: "utils"
+				,location: locationPath + "js/utils"
+			},{
+				name: "widgets"
+				,location: locationPath + "js/widgets"
+			},{
+				name: "app"
+				,location: locationPath + "js"
+				,main: "main"
+			}
+		]
+	}, ["app"]);
+
+})();
 
 
 
