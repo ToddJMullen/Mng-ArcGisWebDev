98bfe79c03766382475e8e8f5e8c0ed62f863d46
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-17 07:17:02 -0500

    symbolUtil.js, Adjust/play with colors. Add log dump of Line & Fill Symbols

--- app/js/utils/symbolUtil.js
+++ app/js/utils/symbolUtil.js
@@ -12,13 +12,14 @@ define([
 	,"esri/symbols/SimpleFillSymbol"
 	,"esri/symbols/SimpleLineSymbol"
 ], function( Color, SimpleFillSymbol, SimpleLineSymbol ){
+	console.log("symbolUtil() SimpleFillSym():", SimpleFillSymbol(), "\nSimpleLineSym():", SimpleLineSymbol() );
 	return {
 		renderSymbol: function(){
 			return new SimpleFillSymbol(
 				SimpleFillSymbol.STYLE_SOLID
 				,new SimpleLineSymbol(
-					SimpleLineSymbol.STYLE_SOLID
-					,new Color([255,55,55])
+					SimpleLineSymbol.STYLE_DASH
+					,new Color([55,155,205])
 					,1
 				)
 				,new Color([128,128,128,0.5])
