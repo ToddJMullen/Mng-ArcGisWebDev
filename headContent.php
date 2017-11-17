817df5e5109b3e06dc391189dbf8156415ea1fe7
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-17 07:35:22 -0500

    Add Bootstrap stylesheet

--- headContent.php
+++ headContent.php
@@ -5,14 +5,14 @@
  * File		: headContent.php
  * Created	: Nov 12, 2017, 7:41:17 PM
  * @author	: Todd Mullen
- * Description : headContent.php
+ * Description : Holds common included elements
  ***********************/
 
 //session_start();
 
 ?>
+<link href="/css/bootstrap.min.css" rel="stylesheet">
 <link href="http://js.arcgis.com/3.11/esri/css/esri.css" rel="stylesheet">
-<!--<link href="http://js.arcgis.com/3.11/js/esri/esri.css" rel="stylesheet">-->
 <link href="/css/app.css" rel="stylesheet">
 
 
