2a6db0d2a73e8ab5603ce8520bc2f5ac58df84cf
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-16 21:11:59 -0500

    MapServices.js, Define a MapServices Dojo module

---
+++ app/js/services/MapServices.js
@@ -0,0 +1,36 @@
+/***********************
+ *	Project		: ArcGisWebDev
+ *	Author		: Todd Mullen
+ *	Document	: MapServices.js
+ *	Created		: Nov 16, 2017, 9:05:31 PM
+ *	Description	:
+ *      Controls
+ ***********************/
+
+define([
+	"esri/layers/FeatureLayer"
+], function(FeatureLayer){
+	
+	return {
+		loadServices: loadServices
+	};
+
+	function loadServices(){
+		var layers = []
+		,censusLayer
+		;
+		censusLayer = new FeatureLayer(
+			"http://services.arcgis.com/V6ZHFr6zdgNZuVG0/"
+			+ "arcgic/rest/services/"
+			+ "CensusLabeorDemo/FeatureService/1"
+		);
+
+		layers.push( censusLayer );
+		return layers;
+	};
+
+});
+
+
+
+
