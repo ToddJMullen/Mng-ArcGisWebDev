2bfd5711c6dcf28e8e8887cc7bc0eb0688118504
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-16 20:59:12 -0500

    app/index.php, Add Script tags for app includes

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ArcGIS v3.11 Web Development</title>
+		<?php require_once '../headContent.php'; ?>
+		<script src="http://js.arcgis.com/3.11compact" type="text/javascript"></script>
+		<script src="js/run.js" type="text/javascript"></script>
+	</head>
+	<body class="app">
+
+		<div id="divMap"></div>
+		<div id="sivMapTools"></div>
+
+	</body>
+</html>
