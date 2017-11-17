50d4e9c60f9cf6dab20cffeb38ba229da5a95ae7
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-16 21:35:35 -0500

    MapController.js, Alter triggered method names to match event signal the MapServices

--- app/js/controllers/MapController.js
+++ app/js/controllers/MapController.js
@@ -22,12 +22,19 @@ define([
 		}
 		,load: function(){
 			var deferred = new Deferred()
-			,mapLoaded = lang.hitch( this, function(){
+//			,mapLoaded = lang.hitch( this, function(){//the map services changes the event/method name that is fired when ready
+			,layersAdded = lang.hitch( this, function(){//mapLoaded() => layersAdded()
 				deferred.resolve( this.map );
 			});
 
 			this.map = new Map( this.options.elem, this.options.mapOptions );
-			on.once( this.map, 'load', mapLoaded );
+
+//			on.once( this.map, 'load', mapLoaded );//event "load" changes also
+			on.once( this.map, 'layers-add-result', layersAdded );
+
+			//not we have a layer(s) to add
+			this.map.addLayers( this.options.layers );
+
 			return deferred.promise;
 		}
 	});
