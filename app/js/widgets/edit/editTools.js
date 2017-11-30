f4fdf1fe775e4f8cef49fcc728b7837c895fafd0
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-30 06:46:36 -0500

    editTools.js, Change Generic Add Request button to be able to handle multiple type buttons. - Import dojo dom-attr & query in order to query the DOM and work with data attributes - Add request type & introspection to determine type on click events - Add prompt for user input before submitting new typed request.

--- app/js/widgets/edit/editTools.js
+++ app/js/widgets/edit/editTools.js
@@ -9,11 +9,13 @@ define([
   ,'dijit/_TemplatedMixin'
   // dom stuff
   ,'dojo/dom-class'
+  ,"dojo/dom-attr"
+  ,"dojo/query"
   ,"dojo/on"
   ,"esri/graphic"
   // template
   ,'text!widgets/edit/editTools.tpl.html'
-], function(declare, lang, array, _WidgetBase, _TemplatedMixin, domClass, on, Graphic, template) {
+], function(declare, lang, array, _WidgetBase, _TemplatedMixin, domClass, domAttr, query, on, Graphic, template) {
 
   return declare([_WidgetBase, _TemplatedMixin], {
 
@@ -24,6 +26,8 @@ define([
     ,map: null
 	,handler: null
 	,requestLayer: null
+	,requestType: null
+//	,domNode: null
 
 
     ,// lifecycle 1
@@ -38,11 +42,20 @@ define([
 	,postCreate: function(){
 		this.handler = on.pausable( this.map, "click", lang.hitch( this, "_addPoint" ) );
 		this.handler.pause();
+		this.own(
+			on(
+				this.domNode//I don't know if this is built-in or if its definition is part of the truncated code
+				,".btn-edit:click"
+				,lang.hitch( this, "_toggleEditButton" )
+			)
+		);
 //		I disabled this binding because I think it is bound in the template & again here
 //		which makes it fire twice (on => off) with one click
 //  data-dojo-attach-event="onclick:_addRequest" <= from template
 //		works with one or the other, not both
-		this.own( this.handler, on( this.editNode, "click", lang.hitch( this, "_addRequest") ) );
+//
+//		this.own( this.handler, on( this.editNode, "click", lang.hitch( this, "_addRequest") ) );
+		//the signature above is different than edit shown in the book
 	}
 
     ,// start widget
@@ -70,21 +83,22 @@ define([
 		var ptMap	= e.mapPoint
 		,ptCensus	= e.graphic
 		,attributes	= {
-//			IssueType		: "New Request"
-//			,RequestDate	:  new Date().getTime()
-//			,CensusTract	: ptCensus.attributes.NAME
+			IssueType		: "New Request"
+			,RequestDate	: new Date().getTime()
+			,CensusTract	: ptCensus.attributes.NAME
+			,description	: prompt("What's the haps here?")
 		}
-		,graphic
-//		,graphic	= new Graphic( ptMap, null, attributes )
-		,description	= prompt("What's the haps here?")
+//		,graphic
+		,graphic	= new Graphic( ptMap, null, attributes )
+//		,description	= prompt("What's the haps here?")
 		;
-		attributes.IssueType	= "New Request";
-		attributes.RequestDate	= new Date().getTime();
-		attributes.CensusTract	= ptCensus.attributes.NAME;
-		attributes.Description	= description;
+//		attributes.IssueType	= "New Request";
+//		attributes.RequestDate	= new Date().getTime();
+//		attributes.CensusTract	= ptCensus.attributes.NAME;
+//		attributes.Description	= description;
 
 		console.log("editTools::_addPoint() attributes:", attributes );
-		graphic = new Graphic( ptMap, null, attributes );
+//		graphic = new Graphic( ptMap, null, attributes );
 
 		console.log("editTools::_addPoint() submitting graphic:", graphic );
 		this.requestLayer
@@ -100,23 +114,36 @@ define([
 
 	}
 
-    ,// private functions
-    _init: function() {
+    ,_init: function() {
 		console.log("_init()");
     }
 
-    ,_toggleEditButton: function() {
-		this.editing = !this.editing;
-		if(this.editing) {
-			console.log("_toggleEditButton(), editing");
-			this.editNode.innerHTML = 'Adding Request';
-			this.handler.resume();//start watching/handling again
-		} else {
-			console.log("_toggleEditButton(), not editing");
-			this.editNode.innerHTML = 'Add Request';
-			this.handler.pause();//stop watching for add events
+    ,_toggleEditButton: function(e) {
+		this.editing		= !this.editing;
+		this.requestType	= "";
+		if( e ){
+			this.requestType	= domAttr.get(e.target, "data-type" );
+			domClass.toggle( e.target, "btn-primary btn-success" );
 		}
-		domClass.toggle(this.editNode, 'btn-primary btn-success');
+
+		if( this.editing ){
+			query(".btn-primary", this.domNode ).removeClass("btn-primary").attr("disabled","disabled");
+			this.handler.resume();
+		} else {
+			query(".btn-edit", this.domNode ).removeClass("btn-success").addClass("btn-primary").removeAttr("disabled");
+			this.handler.pause();
+		}
+
+//		if(this.editing) {
+//			console.log("_toggleEditButton(), editing");
+//			this.editNode.innerHTML = 'Adding Request';
+//			this.handler.resume();//start watching/handling again
+//		} else {
+//			console.log("_toggleEditButton(), not editing");
+//			this.editNode.innerHTML = 'Add Request';
+//			this.handler.pause();//stop watching for add events
+//		}
+//		domClass.toggle(this.editNode, 'btn-primary btn-success');
     }
 
   });
